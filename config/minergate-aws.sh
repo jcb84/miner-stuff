af84df92e4d7bff88d33812a0b844a2a529fd91e
mt <mtemimi@gmail.com>
2017-11-27 16:54:23 +0100

    Added minergate config for aws virtual node

---
+++ config/minergate-aws.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#apt-get update
+
+MAIL="mail"
+
+export LOG_MG="/var/log/xmrig"
+export LOG_MG_INSTALL="${LOG_XMRIG}_install"
+
+echo > ${LOG_MG}
+echo > ${LOG_MG_INSTALL}
+
+wget -O /tmp/mine.deb https://minergate.com/download/deb-cli >> ${LOG_MG_INSTALL}
+dpkg -i /tmp/mine.deb >> ${LOG_MG_INSTALL}
+rm /tmp/mine.deb
+minergate-cli -user ${MAIL} -xmr 2>&1 > ${LOG_MG}
