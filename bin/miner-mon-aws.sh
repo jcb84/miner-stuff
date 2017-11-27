79f09883d7b8d14137621b4306f356aac5f8ac81
mt <mtemimi@gmail.com>
2017-11-27 17:36:48 +0100

    Enhanced monitoring miner logfile

--- bin/miner-mon-aws.sh
+++ bin/miner-mon-aws.sh
@@ -4,8 +4,18 @@ APP_TOKEN=""
 USER_TOKEN=""
 MESSAGE=$(hostname)
 
-/usr/bin/curl -s \
+FILE_TO_MONITOR="/var/log/xmrig"
+
+function send_pushover_message()
+{
+    /usr/bin/curl -s \
 	      --form-string "token=${APP_TOKEN}" \
 	      --form-string "user=${USER_TOKEN}" \
 	      --form-string "message=Miner is down on ${MESSAGE}" \
 	        https://api.pushover.net/1/messages.json
+}
+
+FOUND_FILE=$(/usr/bin/find /var/log -name xmrig -mmin 5)
+
+foreach file in ${FOUND_FILE}; do send_pushover_message; done
+
