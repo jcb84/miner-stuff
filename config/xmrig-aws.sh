aa3c173fad74a04813dd51dab3befaf0774d4be0
mt <mtemimi@gmail.com>
2017-11-27 16:14:42 +0100

    Init

---
+++ config/xmrig-aws.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+export LOG_XMRIG="/var/log/xmrig"
+export LOG_XMRIG_INSTALL="${LOG_XMRIG}_install"
+export TGT_DIR="/opt/github/"
+
+XMR_ADDRESS="42XXKCKA3Py1s96xZhEbSEgbxrzDRyiTwEvJRB7dZNaMFPXicziKPz8Ua7wkPaHQsgX8LKvnHmLm11kn8Vig7SsMPEyyygK"
+
+echo > ${LOG_XMRIG_INSTALL}
+echo > ${LOG_XMRIG}
+
+apt-get update >> ${LOG_XMRIG_INSTALL}
+apt-get -y install git build-essential cmake libuv1-dev libmicrohttpd-dev >> ${LOG_XMRIG_INSTALL}
+mkdir -p ${TGT_DIR} >> ${LOG_XMRIG_INSTALL}
+cd ${TGT_DIR}
+git clone https://github.com/xmrig/xmrig.git xmrig  >> ${LOG_XMRIG_INSTALL}
+cd xmrig
+mkdir build  >> ${LOG_XMRIG_INSTALL}
+cd build
+cmake ..  >> ${LOG_XMRIG_INSTALL}
+sleep 2
+make  >> ${LOG_XMRIG_INSTALL}
+sleep 2
+
+nohup ${TGT_DIR}/xmrig/build/xmrig -o de.minexmr.com:4444 -u ${XMR_ADDRESS} -p x -k 2>&1 > ${LOG_XMRIG}
+
